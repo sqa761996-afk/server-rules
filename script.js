902bb00946f068032fe615940850935fd992c3fc
sqa761996-afk <sqa761996@gmail.com>
2026-02-03 20:32:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function toggleMenu(){
   let nav = document.getElementById('nav');
+  let overlay = document.getElementById('nav-overlay');
   if(nav.style.left === "0px"){
-    nav.style.left = "-250px";
+    nav.style.left = "-300px";
+    overlay.style.display = "none";
   } else {
     nav.style.left = "0px";
+    overlay.style.display = "block";
   }
+}
+function closeMenu(){
+  document.getElementById('nav').style.left = "-300px";
+  document.getElementById('nav-overlay').style.display = "none";
 }
