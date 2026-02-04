225150dbb37de8355a96b7c24cfe20f43cc7e076
RT on top <sqa761996@gmail.com>
2026-02-04 13:42:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-const nav = document.getElementById('nav');
-const overlay = document.getElementById('nav-overlay');
-const menuBtn = document.getElementById('menu-btn');
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+  const overlay = document.getElementById("nav-overlay");
 
-menuBtn.addEventListener('click', () => {
-  nav.classList.toggle('open');
-  overlay.style.display = nav.classList.contains('open') ? 'block' : 'none';
-  menuBtn.classList.toggle('open');
-});
-
-function closeMenu(){
-  nav.classList.remove('open');
-  overlay.style.display = "none";
-  menuBtn.classList.remove('open');
+  menu.classList.toggle("open");
+  overlay.style.display = menu.classList.contains("open") ? "block" : "none";
 }
