2285b72305f44b9675ffccc1c43aa319c92f7688
RT on top <sqa761996@gmail.com>
2026-02-04 14:05:29 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-  const menu = document.getElementById("navMenu");
-  const overlay = document.getElementById("nav-overlay");
-
-  menu.classList.toggle("open");
-  overlay.style.display = menu.classList.contains("open") ? "block" : "none";
-}
