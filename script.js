cd37e2451b5d317249e849599088eac84b82209c
RT on top <sqa761996@gmail.com>
2026-02-04 14:08:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+  const overlay = document.getElementById("nav-overlay");
+  menu.classList.toggle("open");
+  overlay.style.display = menu.classList.contains("open") ? "block" : "none";
+}
+
+async function updateMemberCount() {
+  const serverID = "1453314826322317366"; // ID السيرفر
+  const endpoint = `https://discord.com/api/guilds/${serverID}/widget.json`;
+  try {
+    const res = await fetch(endpoint);
+    if(res.ok){
+      const data = await res.json();
+      document.getElementById("memberCount").textContent = data.presence_count || 0;
+    } else {
+      console.log("خطأ في جلب البيانات");
+    }
+  } catch(e){
+    console.log("فشل الاتصال بالـ API", e);
+  }
+}
+updateMemberCount();
+setInterval(updateMemberCount, 10000);
